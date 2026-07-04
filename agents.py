1f2be56b106ab8384bab782c240a16fcb3ac4af5
Atharva Jadhav <jadhavatharva499@gmail.com>
2026-07-04 23:24:50 +0530

    Added react agents

---
+++ agents.py
@@ -0,0 +1,78 @@
+import os
+from langchain.agents import create_agent
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from tools import web_search, scrape_url
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
+
+# model setup
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-R1",
+    task="text-generation",
+    max_new_tokens=256,
+    huggingfacehub_api_token=token,
+)
+
+model = ChatHuggingFace(llm=llm)
+
+def build_search_Agent():
+    return create_agent(
+        model=llm,
+        tools=[web_search]
+    )
+
+def build_reader_agent():
+    return create_agent(
+        model=llm,
+        tools=[scrape_url]
+    )
+
+writer_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are an expert research writer. Write clear, structured and insightful reports."),
+    ("human", """Write a detailed research report on the topic below.
+     
+Topic: {topic}
+
+Research Gathered:
+{research}
+     
+Structure the report as:
+- Introduction
+- Key Findings (minimum 3 well-explained points)
+- Conclusion
+- Sources (list all URLs found in the research)
+     
+Be detailed, factual and professional."""),
+])
+
+writer_chain = writer_prompt | llm | StrOutputParser()
+
+critic_prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a sharp and constructive research critic. Be honest and specific."),
+    ("human", """Review the research report below and evaulate it strictly.
+     
+Report:
+{report}
+     
+Respond in this exact format:
+     
+Score: X/10
+     
+Strengths:
+- ...
+- ...
+     
+Areas to Improve:
+- ...
+- ...
+
+One line verdict:
+..."""),
+])
+
+critic_chain = critic_prompt | llm | StrOutputParser()
